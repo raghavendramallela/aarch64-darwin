68992e8189b9b93f0d7da271c2debfcf5cd9756a
raghavendrmallela <mrgdevops@gmail.com>
2024-11-19 06:06:24 +0530

    added starship zshrc

---
+++ configfiles/nix/flake.nix
@@ -0,0 +1,82 @@
+# ~/.config/nix/flake.nix
+
+{
+  description = "My system configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+        url = "github:LnL7/nix-darwin";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+        url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  let
+    configuration = {pkgs, ... }: {
+
+        services.nix-daemon.enable = true;
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+        # Create /etc/zshrc that loads the nix-darwin environment.
+        programs.zsh.enable = true;
+        security.pam.enableSudoTouchIdAuth = true;
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility. please read the changelog
+        # before changing: `darwin-rebuild changelog`.
+        system.stateVersion = 4;
+
+        # The platform the configuration will be used on.
+        # If you're on an Intel system, replace with "x86_64-darwin"
+        nixpkgs.hostPlatform = "aarch64-darwin";
+
+        # Declare the user that will be running `nix-darwin`.
+        users.users.raghu = {
+            name = "raghu";
+            home = "/Users/raghu";
+        };
+        nix.configureBuildUsers = true;
+        nix.useDaemon = true;
+        ids.gids.nixbld = 350;
+
+        environment.systemPackages = [
+          # pkgs.vscode
+          pkgs.spotify
+          pkgs.neovim
+          pkgs.starship
+          pkgs.nushell
+          pkgs.git
+          pkgs.go
+          pkgs.gh
+          pkgs.bat
+          pkgs.kind
+          # pkgs.firefox
+          pkgs.google-chrome
+          pkgs.fastfetch
+          pkgs.iterm2
+          pkgs.kubectl
+          pkgs.direnv
+        ];
+    };
+
+  in
+  {
+    darwinConfigurations."ecosystem" = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+         configuration
+         home-manager.darwinModules.home-manager  {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.verbose = true;
+            home-manager.users.raghu = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
